5b11beb7b8123ba0b6745138e3e88e4d2b25956e
teno <teno@peppercon.co.kr>
2024-11-24 20:32:24 +0900

    특정단어 찾기

--- cmd/golang-findword/main.go
+++ cmd/golang-findword/main.go
@@ -2,11 +2,97 @@
 package main
 
 import (
-    "fmt"
-	"golang-findword/pkg/exinit"
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+type FindInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
 func main() {
-	fmt.Println("main function")
-	exinit.PrintD()
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 실행 인수가 필요합니다. ex) make run word filepath")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+	findInfos := []FindInfo{}
+
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		fmt.Println("-------------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("-------------------------")
+		fmt.Println()
+	}
+}
+
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	findInfos := []FindInfo{}
+
+	filelist, err := GetFileList(path)
+	if err != nil {
+		fmt.Println("파일 경로가 잘못되었습니다. err:", err, "path:", path)
+		return findInfos
+	}
+
+	for _, filename := range filelist {
+		findInfos = append(findInfos, FindWordInFile(word, filename))
+	}
+	return findInfos
+}
+
+func FindWordInFile(word, filename string) FindInfo {
+	findInfo := FindInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다.", filename)
+		return findInfo
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	return findInfo
+}
+
+func PrintFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다.", filename)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
